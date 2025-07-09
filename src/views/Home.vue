6888e55142a2274bfd8b833c74c10cccae09f57d
greenhc502 <greenhc502@gmail.com>
2025-07-09 12:41:07 +0900

    4교시 중간

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { getItems } from '@/Services/itemService';
-import { onMounted } from 'vue';
+import { onMounted, reactive } from 'vue';
+import Card from '@/components/Card.vue';
+const state = reactive({
+  items: [],
+});
 
 onMounted(async () => {
-
-
   const res = await getItems();
-  console.log(res.data);
+  if (res.status !== 200) {
+    return;
+  }
+  console.log('res.dat:', res.data);
+  state.items = res.data;
+
+  // const res = await getItems();
+  // console.log(res.data);
 });
 
 /*
@@ -17,8 +26,16 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="container">
-    <h1 class="mt-5">Hello, Home</h1>
+  <div class="home">
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
+          <div class="col" v-for="item in state.items">
+            <card :item="item" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
